d8a841719f7d64ba3f09f8a353d2a53a2f4e57b1
pol17 <pol17@ukr.net>
2020-01-08 16:06:47 +0200

    class HW1 arrays

---
+++ src/Classroom_HomeWork/HW1_arrays.java
@@ -0,0 +1,8 @@
+package Classroom_HomeWork;
+
+public class HW1_arrays {
+    int[] number = {2,4,26,7,28,10,9,20,33};
+        for (int i = 0; i < number.length; i+=1) {
+        System.out.println(number[i]);
+    }
+}
