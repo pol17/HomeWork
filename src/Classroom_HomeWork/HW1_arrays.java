8d0d8406082b1e81750046302d6c6e0066588346
Vasiliy_Gnatiuk <pol17@ukr.net>
2020-01-09 20:35:01 +0200

    HW1 example added

--- src/Classroom_HomeWork/HW1_arrays.java
+++ src/Classroom_HomeWork/HW1_arrays.java
@@ -1,8 +1,10 @@
 package Classroom_HomeWork;
 
 public class HW1_arrays {
-    int[] number = {2,4,26,7,28,10,9,20,33};
-        for (int i = 0; i < number.length; i+=1) {
-        System.out.println(number[i]);
+    public static void main(String[]args){
+    int[] numbers = {2,4,26,7,28,10,9,20,33};
+        for (int i = 0; i < numbers.length; i = i + 1) { //int i = 0 - начинаем с 1го символа
+            System.out.println(numbers[i]);
+        }
     }
 }
